aa79b09d62be35d15034fc8dc19805aff60652d7
Mark Gowen <mark.gowen78@gmail.com>
2022-11-09 13:12:42 -0800

    Details of Ramen in ramen-detail div

--- src/index.js
+++ src/index.js
@@ -8,9 +8,27 @@ function onPageLoad() {
             const ramenImgs = document.createElement("img")
 
             ramenImgs.src = element.image
-
+            
             ramenMenu.appendChild(ramenImgs)
+            ramenDetails(element, ramenImgs)
         })
+    })
+}
+
+function ramenDetails(element, ramenImgs) {
+    const ramenName = document.querySelector(".name")
+    const rest = document.querySelector(".restaurant")
+    const img = document.querySelector(".detail-image")
+    const rating = document.querySelector("#rating-display")
+    const comment = document.querySelector("#comment-display")
+    
+    
+    ramenImgs.addEventListener("click", (e) => {
+        ramenName.textContent = element.name
+        img.src = element.image 
+        rest.textContent = element.restaurant
+        rating.textContent = element.rating
+        comment.textContent = element.comment
     })
 }
 
