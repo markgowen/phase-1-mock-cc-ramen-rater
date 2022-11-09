a6a5ffd37933132017785510fd91c53215b316d5
Mark Gowen <mark.gowen78@gmail.com>
2022-11-09 12:13:57 -0800

    Compiled ramen images into ramen-menu div

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // write your code here
+function onPageLoad() {
+    fetch('http://localhost:3000/ramens')
+    .then(res => res.json())
+    .then(function(data) {
+        data.forEach((element) => {
+            const ramenMenu = document.querySelector("#ramen-menu")
+            const ramenImgs = document.createElement("img")
+
+            ramenImgs.src = element.image
+
+            ramenMenu.appendChild(ramenImgs)
+        })
+    })
+}
+
+onPageLoad()
